e4a6330b8c38e884dc707cb0c210bb108da750ff
GanievaKarina <ganievakarina160504@gmail.com>
2021-03-30 22:40:38 +0500

    Update script.js

--- script.js
+++ script.js
@@ -1,42 +1,45 @@
 let body = document.body;
 let url = window.location.toString();
-
+let text = document.querySelector(".text");
 const getNameFromUrl = (url) => {
-  let getUrl = url.split('=');
+  let getUrl = url.split("=");
   let nameUrl = getUrl[1];
-  if(nameUrl == undefined) {
-  nameUrl = 'GanievaKarina';
+  if (nameUrl == undefined) {
+    nameUrl = "GanievaKarina";
   }
-return nameUrl;
-}
+  return "Persovt";
+};
 
-fetch(` https://api.github.com/users/${getNamefromUrl(url)}`)
-  .then(res => res.json())
-  .then(json => {
+fetch(` https://api.github.com/users/Persovt`)
+  .then((res) => res.json())
+  .then((json) => {
     console.log(json.avatar_url);
     console.log(json.name);
     console.log(json.bio);
     console.log(json.html_url);
 
-    //let photo = new Photo();
-  //  photo.scr = json.avatar_url;
-  //  body.append(photo);
+    // let photo = new Photo();
+    // photo.scr = json.avatar_url;
+    // body.append(photo);
 
-    let name = document.createElement('p');
-    if (json.name != null){
-      name.innerHTML = json.name;
-    } else{
-      name.innerHTML = 'Информация о пользователе не доступна';
+console.log(text)
+    if (json.name != null) {
+      text.innerHTML = json.name;
+    } else {
+      text.innerHTML = "Информация о пользователе не доступна";
     }
-    body.append(name);
-    name.addEventListener("click", () => window.location = 'https://webheroschool.github.io/fetchh/');
 
-    let bio = document.createElement('p');
-    if (json.bio != null){
+    text.addEventListener(
+      "click",
+      () => (window.location = "https://webheroschool.github.io/fetchh/")
+    );
+
+    let bio = document.createElement("p");
+    if (json.bio != null) {
       bio.innerHTML = json.bio;
-    } else{
-      bio.innerHTML = 'Информация о пользователе не доступна';
+    } else {
+      bio.innerHTML = "Информация о пользователе не доступна";
     }
     body.append(bio);
   })
-  .catch(err => 'Информация о пользователе не доступна');
+  .catch((err) => "Информация о пользователе не доступна");
