47e6f1b21f8108e299403bcb8175ee59c4d049ed
GanievaKarina <ganievakarina160504@gmail.com>
2021-04-01 23:06:41 +0500

    Update script.js

--- script.js
+++ script.js
@@ -1,45 +1,67 @@
 let body = document.body;
-let url = window.location.toString();
-let text = document.querySelector(".text");
-const getNameFromUrl = (url) => {
-  let getUrl = url.split("=");
-  let nameUrl = getUrl[1];
-  if (nameUrl == undefined) {
-    nameUrl = "GanievaKarina";
-  }
-  return nameUrl;
+let preloader = document.getElementById("preloader_preload");
+
+function fadeOutnojquery(el) {
+   el.style.opacity = 1;
+   var interpreloader = setInterval(function () {
+      el.style.opacity = el.style.opacity - 0.05;
+      if (el.style.opacity <= 0.05) {
+         clearInterval(interpreloader);
+         preloader.style.display = "none";
+      }
+   }, 16);
+}
+
+window.onload = function () {
+   setTimeout(function () {
+      fadeOutnojquery(preloader);
+   }, 3000);
 };
-
-fetch(` https://api.github.com/users/GanievaKarina`)
-  .then((res) => res.json())
-  .then((json) => {
-    console.log(json.avatar_url);
-    console.log(json.name);
-    console.log(json.bio);
-    console.log(json.html_url);
-
-    let photo = new Image();
-    photo.scr = json.avatar_url;
-    body.append(photo);
-
-console.log(text)
-    if (json.name != null) {
-      text.innerHTML = json.name;
-    } else {
-      text.innerHTML = "Информация о пользователе не доступна";
-    }
-
-    text.addEventListener(
-      "click",
-      () => (window.location = "https://webheroschool.github.io/fetchh/")
-    );
-
-    let bio = document.createElement("p");
-    if (json.bio != null) {
-      bio.innerHTML = json.bio;
-    } else {
-      bio.innerHTML = "Информация о пользователе не доступна";
-    }
-    body.append(bio);
+window.setTimeout(function () {
+  document.body.classList.add('loaded');
+  let body = document.body;
+  let string = window.location.search;
+  let url = 'https://api.github.com/users/GanievaKarina';
+  const date = new Date();
+  const getDate = new Promise((resolve, reject) => {
+    setTimeout(() => date ? resolve(date) : reject("Ошибка"), 100)
   })
-  .catch((err) => "Информация о пользователе не доступна");
+  const getUrl = new Promise((resolve, reject) => {
+    setTimeout(() => url ? resolve(url) : reject("Ошибка URL!"), 100)
+  })
+
+   Promise.all([getUrl, getDate])
+      .then(([url, date]) => fetch(url))
+      .then(res => res.json())
+      .then(json => {
+          console.log(json.avatar_url);
+          console.log(json.name);
+          console.log(json.bio);
+          console.log(json.html_url);
+
+          let photo = new Image();
+          photo.src = json.avatar_url;
+          body.append(photo);
+
+          let name = document.createElement('p');
+          if (json.name != null) {
+              name.innerHTML = json.name;
+          } else {
+              name.innerHTML = 'Информация о пользователе недоступна';
+          }
+          body.append(name);
+          name.addEventListener("click", () => window.location = json.html_url);
+
+          let bio = document.createElement('p');
+          if (json.bio != null) {
+              bio.innerHTML = json.bio;
+          } else {
+              bio.innerHTML = 'Информация о пользователе недоступна';
+          }
+          body.append(bio);
+          body.append(date)
+
+    })
+
+    .catch(err => console.log('Информация о пользователе недоступна'));
+ }, 3000);
